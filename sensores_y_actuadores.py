b077eb596e63d4ec7ef84223cdbd33022879f6fc
jpcedano <83967893+jpcedano@users.noreply.github.com>
2022-09-14 18:47:25 -0500

    Update sensores_y_actuadores.py

--- sensores_y_actuadores.py
+++ sensores_y_actuadores.py
@@ -1,8 +1,10 @@
+import time
+import grovepi
+from grovepi import *
+
 
 # sensor touch
 #####################################################################
-import time
-import grovepi
  
 # Connect the Grove Touch Sensor to digital port D2
 # SIG,NC,VCC,GND
@@ -18,4 +20,46 @@ while True:
     except IOError:
         print ("Error")
     ###################################################################
-    
+# sensor temperatura
+########################################################################
+
+# Connect the Grove Temperature Sensor to analog port A0
+# SIG,NC,VCC,GND
+sensor = 0
+ 
+while True:
+try:
+temp = grovepi.temp(sensor,'1.1')
+print "temp =", temp
+time.sleep(.5)
+ 
+except KeyboardInterrupt:
+break
+except IOError:
+print "Error"
+
+#############################################################################
+#LED Socket
+############################################################################
+# GrovePi LED Blink example
+ 
+# Connect the Grove LED to digital port D4
+led = 4
+ 
+pinMode(led,"OUTPUT")
+time.sleep(1)
+ 
+while True:
+    try:
+        #Blink the LED
+        digitalWrite(led,1)     # Send HIGH to switch on LED
+        time.sleep(1)
+ 
+        digitalWrite(led,0)     # Send LOW to switch off LED
+        time.sleep(1)
+ 
+    except KeyboardInterrupt:   # Turn LED off before stopping
+        digitalWrite(led,0)
+        break
+    except IOError:             # Print "Error" if communication error encountered
+        print "Error"
