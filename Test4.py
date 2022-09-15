08d9f4429398447f8767631fc0adcfddeb3f56f9
DevasNAI <A01245418@itesm.mx>
2022-09-15 17:18:22 -0500

    tempReading change

--- Test4.py
+++ Test4.py
@@ -190,6 +190,5 @@ def delTemp(id):
 #         return jsonify({"error": e})
 
 if __name__ == "__main__":
-    while(True):
-        tempReading()
-        app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000)
+    tempReading()
